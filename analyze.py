491509f8d4f0606fb1a4832fd6fee71288367299
David Calvo <dcalvo2@jhu.edu>
2021-06-26 01:42:00 -0400

    Parallelize analysis.py

--- analyze.py
+++ analyze.py
@@ -4,6 +4,8 @@ import urllib.request
 from datetime import datetime, timedelta
 
 from scipy.special import softmax
+from torch.multiprocessing import Pool, cpu_count
+from tqdm import tqdm
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
 
@@ -18,35 +20,43 @@ def preprocess(text):
     return " ".join(new_text)
 
 
-MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
+def setup_model():
+    MODEL = f"cardiffnlp/twitter-roberta-base-sentiment"
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL)
+    tokenizer = AutoTokenizer.from_pretrained(MODEL)
 
-# download label mapping
-labels = []
-mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/sentiment/mapping.txt"
-with urllib.request.urlopen(mapping_link) as f:
-    html = f.read().decode("utf-8").split("\n")
-    csvreader = csv.reader(html, delimiter="\t")
-labels = [row[1] for row in csvreader if len(row) > 1]
+    # download label mapping
+    labels = []
+    mapping_link = f"https://raw.githubusercontent.com/cardiffnlp/tweeteval/main/datasets/sentiment/mapping.txt"
+    with urllib.request.urlopen(mapping_link) as f:
+        html = f.read().decode("utf-8").split("\n")
+        csvreader = csv.reader(html, delimiter="\t")
+    labels = [row[1] for row in csvreader if len(row) > 1]
 
-# PT
-model = AutoModelForSequenceClassification.from_pretrained(MODEL)
-tokenizer.save_pretrained(MODEL)
-model.save_pretrained(MODEL)
+    # PT
+    model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+    tokenizer.save_pretrained(MODEL)
+    model.save_pretrained(MODEL)
+
+    return model, tokenizer
 
 
-with open("sentiment.csv", "w", encoding="utf-8") as f:
-    write = csv.writer(f)
+def analyze_dict(date, model, tokenizer):
+    with open(f"tweets_sentiment_{date}.csv", "w", encoding="utf-8") as f:
+        write = csv.writer(f)
+        fields = ["Text", "Retweets", "Negative", "Neutral", "Positive"]
+        write.writerow(fields)
 
-    fields = ["Text", "Retweets", "Negative", "Neutral", "Positive"]
-    write.writerow(fields)
-
-    for day in range(7, 0, -1):
-        date = (datetime.today() - timedelta(days=day)).strftime("%Y-%m-%d")
         tweet_dict = pickle.load(open(f"tweets_dict_{date}.pickle", "rb"))
 
         # FULL instance of Tweet
+        # for tweet in tqdm(
+        #     tweet_dict.values(),
+        #     desc=f"Analyzing Tweets for {date}",
+        #     total=len(tweet_dict),
+        #     leave=False,
+        # ):
+        print(f"Beginning analysis for {date}...")
         for tweet in tweet_dict.values():
             text = preprocess(tweet.full_text)
             encoded_input = tokenizer(text, return_tensors="pt")
@@ -56,10 +66,28 @@ with open("sentiment.csv", "w", encoding="utf-8") as f:
             out_row = [tweet.full_text, tweet.retweet_count, *scores, tweet.id]
             write.writerow(out_row)
 
+    print(f"Analysis for {date} complete.")
+
+
+if __name__ == "__main__":
+    # Out of curiosity, really.
+    print("Number of processors: ", cpu_count())
+
+    # Create the model and tokenizer to share amongst processes
+    print("Setting up model...")
+    model, tokenizer = setup_model()
+    args = []
+    # Package the args into tuples for each process
+    for day in range(7, 0, -1):
+        date = (datetime.today() - timedelta(days=day)).strftime("%Y-%m-%d")
+        args += [(date, model, tokenizer)]
+    print("Initiating analysis...")
+    with Pool(7) as p:
+        p.starmap(analyze_dict, args)
 
 # ranking = np.argsort(scores)
 # ranking = ranking[::-1]
-# print(scores, ranking, labels)
+# print(scores, ranking, labels)x
 # for i in range(scores.shape[0]):
 #     l = labels[ranking[i]]
 #     s = scores[ranking[i]]
